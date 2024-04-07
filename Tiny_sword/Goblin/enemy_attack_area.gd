5077b1f374f7ee6e98d53ac337a6d54dfa7a0a39
peres-b <peres.b@hotmail.com>
2024-04-07 17:47:50 +0200

    update
    fixed ysorting:
    trees
    
    remade base level
    
    reworked level_2

--- Tiny_sword/Goblin/enemy_attack_area.gd
+++ Tiny_sword/Goblin/enemy_attack_area.gd
@@ -6,10 +6,10 @@ extends Area2D
 func on_body_entered(body):
 	if body.is_in_group("Player"):
 		body.take_damage(damage)
-		#body.push_back()
+		
 	
 
 
 func on_lifetime_timeout() -> void:
 	queue_free()
-	pass # Replace with function body.
+	
